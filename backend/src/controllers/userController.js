016e3eb5c8b1803aacfb59f0f21de734f52aa372
syaila02 <syailzah02r@gmail.com>
2026-05-04 18:16:48 +0700

    complete profile avatar and pencil overlay

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,12 +1,40 @@
 import User from "../models/User.js";
+import { syncUserAvatarInRooms } from "../sockets/gameSocket.js";
+
+const allowedAvatars = new Set(Array.from({ length: 12 }, (_, index) => `Avatar${index + 1}.png`));
+
+const userPayload = (user) => ({
+  id: user._id,
+  username: user.username,
+  email: user.email,
+  totalMatch: user.totalMatch,
+  win: user.win,
+  lose: user.lose,
+  winrate: user.winrate,
+  totalValidWords: user.totalValidWords,
+  avatar: user.avatar || "Avatar1.png"
+});
 
 export const profile = async (req, res) => {
-  res.json({ user: req.user });
+  res.json({ user: userPayload(req.user) });
+};
+
+export const updateAvatar = async (req, res) => {
+  const { avatar } = req.body;
+  if (!allowedAvatars.has(avatar)) {
+    return res.status(400).json({ message: "Avatar tidak valid" });
+  }
+
+  req.user.avatar = avatar;
+  await req.user.save();
+  await syncUserAvatarInRooms(req.app.get("io"), req.user._id, avatar);
+
+  res.json({ user: userPayload(req.user) });
 };
 
 export const leaderboard = async (_req, res) => {
   const users = await User.find()
-    .select("username totalMatch win lose winrate totalValidWords")
+    .select("username totalMatch win lose winrate totalValidWords avatar")
     .sort({ winrate: -1, win: -1, totalMatch: 1 })
     .limit(25);
 
